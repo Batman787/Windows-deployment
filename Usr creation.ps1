313d2253c494cf9e742c635cb354719e41f2a0e0
Batman787 <36492037+Batman787@users.noreply.github.com>
2022-03-09 11:01:19 +0000

    Add files via upload

---
+++ Usr creation.ps1
@@ -0,0 +1,21 @@
+﻿Import-Module ActiveDirectory
+
+$passwd = ConvertTo-SecureString "Admin1!" -AsPlainText -Force
+
+$users = Import-Csv 'C:\Users\Administrator\Desktop\Copy of Initech Staff List.csv'
+
+ForEach ($user in $users) {
+    $fname = $user.'First Name'
+    $lname = $user.'Last Name'
+    $uname = $user.Username
+    $title = $user.Title
+    $empnumber = $user.'Employee #'
+    $ou = $user.OU
+    $manager = $user.'Reporting To'
+    $homedir = \\WIN-JJ7Q1UCROEN.Intech.net\User_share$\%UserName%
+
+
+    New-ADUser -Name "$fname $lname" -GivenName $fname -Surname $lname -UserPrincipalName "$fname $lname" -Path $ou -AccountPassword $passwd -ChangePasswordAtLogon $True -Description "$empnumber $title" -Enabled $True -Title $title -EmployeeNumber $empnumber -HomeDrive 'Z:' -HomeDirectory \\WIN-JJ7Q1UCROEN.Intech.net\User_share$\%UserName%
+
+    echo "account created for  $empnumber $fname $lname $uname $title in $ou"
+ }
